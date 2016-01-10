14e478f4c96adff1cdfeee2c3fa225221f3ac690
Centurion256 <Vkuchynskyy@gmail.com>
2016-01-10 12:54:54 +0200

    Created by Coding Ground Client

---
+++ JavaTraining/Volume.java
@@ -0,0 +1,20 @@
+public class Volume{
+
+     public static void main(String []args){
+        int a;
+        int b;
+        int h;
+        int V;
+        
+        System.out.println("Wwedyt Dowzshynu");
+        a=Integer.parseInt(System.console().readLine());
+        System.out.println("Wwedyt Shyrynu");
+        b=Integer.parseInt(System.console().readLine());
+        System.out.println("Wwedyt Wusoty");
+        h=Integer.parseInt(System.console().readLine());
+        
+        V=a*b*h;
+        System.out.println("Ploshtsha ye " + V);
+        
+     }
+}
