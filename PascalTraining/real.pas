ea39ac715999adaacc473045ad3ec4d50b6d8a8e
Centurion256 <Vkuchynskyy@gmail.com>
2016-01-10 14:57:22 +0200

    Created by Coding Ground Client

---
+++ PascalTraining/real.pas
@@ -0,0 +1,18 @@
+Program Volume;
+var
+  a,b,h,V: real;
+begin
+
+  0
+  writeln('Введіть довжину');
+  read(a);
+  writeln('Введіть ширину');
+  read(b);
+  writeln('Введіть висоту');
+  read(h);
+  
+  V:=a*b*h;
+  
+  writeln('Площа кімнати складає ', V);
+
+end.
