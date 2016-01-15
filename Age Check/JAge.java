a91bd03242c01b4a538aa689d3d2be6e70e8b11f
Centurion256 <Vkuchynskyy@gmail.com>
2016-01-15 18:30:37 +0200

    Update JAge.java

--- Age Check/JAge.java
+++ Age Check/JAge.java
@@ -1,40 +1,28 @@
 public class JAge{
 
-static int Age; 
-
-     public static void main(String []args){
-         
+    public static void main(String []args){
+        
         System.out.println("Insert your age");
-        Age=Integer.parseInt(System.console().readLine());
-       
-if (Age<1)
-{
-  System.out.println("The number is negative or 0"); 
+        int Age = Integer.parseInt(System.console().readLine());
+        
+        if (Age<1){
+            System.out.println("The number is negative or 0");
+        }
+        else if (Age<7){
+            System.out.println("Preschool kid");
+        }
+        else if (Age<17){
+            System.out.println("Scoolchild");
+        }
+        else if (Age<22){
+            System.out.println("Student");
+        }
+        else if (Age<60){
+            System.out.println("Work-entitled Person");
+        }
+        else{
+            System.out.println("Senior");
+        }
+    }
 }
-else
-    if (Age<7)
-{
-  System.out.println("Preschool kid");  
-}
-else 
-    if (Age<17)
-{
-  System.out.println("Scoolchild");
-}
-else
-    if (Age<22)
-{
-  System.out.println("Student");
-}
-else
-    if (Age<60)
-{
-  System.out.println("Work-entitled Person");
-}
-else
-    if (Age>59)
-{
-  System.out.println("Senior");
-}
-     }
-}
+
