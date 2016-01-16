b2ddaa69503ffc3ca710b15c99650719cc070fc4
Centurion256 <Vkuchynskyy@gmail.com>
2016-01-16 11:50:45 +0200

    Update JBirthDate.java

--- Age Check/JBirthDate.java
+++ Age Check/JBirthDate.java
@@ -11,51 +11,51 @@ public class JBirthDate{
         
     switch(m){
     case 1:  
-        System.out.println(d , "/january/" , y);
+        System.out.println(d + "/january/" + y);
         break;
         
     case 2:
-        System.out.println(d , "february" , y);
+        System.out.println(d + "february" + y);
         break;
         
     case 3:
-        System.out.println(d , "/march/" , y);
+        System.out.println(d + "/march/" + y);
         break;
         
     case 4:
-        System.out.println(d , "/april/" , y);
+        System.out.println(d + "/april/" + y);
         break;
         
     case 5:
-        System.out.println(d , "/may/" , y);
+        System.out.println(d + "/may/" + y);
         break;
         
     case 6:
-        System.out.println(d , "/june/" , y);
+        System.out.println(d + "/june/" + y);
         break;
         
     case 7:
-        System.out.println(d , "/july/" , y);
+        System.out.println(d + "/july/" + y);
         break;
         
     case 8:
-        System.out.println(d , "/august/" , y);
+        System.out.println(d + "/august/" + y);
         break;
         
     case 9:
-        System.out.println(d , "/september/" , y);
+        System.out.println(d + "/september/" + y);
         break;
         
     case 10:
-        System.out.println(d , "/october/" , y);
+        System.out.println(d + "/october/" + y);
         break;
         
     case 11:
-        System.out.println(d , "/november/" , y);
+        System.out.println(d + "/november/" + y);
         break;
         
     case 12:
-        System.out.println(d , "/december/" , y);
+        System.out.println(d + "/december/" + y);
         break;
     default:    
         if (m<0&&m>12){
