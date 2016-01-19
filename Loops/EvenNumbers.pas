79eac21cd0b7d73d44396d9a0b94444738f22b0b
Centurion256 <Vkuchynskyy@gmail.com>
2016-01-19 15:11:36 +0200

    Update EvenNumbers.pas

--- Loops/EvenNumbers.pas
+++ Loops/EvenNumbers.pas
@@ -1,14 +1,13 @@
 Program EvenNumbers;
 var
-    i,k,S : Integer;
+    i : Integer;
+    k : Integer;
     
 begin
     writeln('Вкажіть значення k');
     read(k);
-    S:=0;
-    
-    for i:= 1 to k/2 do
-        {S:= S+i;
-        i:= i*2;}
-        writeln('Hello World ',i*2);
+   
+
+    for i:= 1 to k div 2 do
+        writeln(i*2);
 end.
