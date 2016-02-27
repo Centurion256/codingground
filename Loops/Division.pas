cb85694006706d3a772151efaebdb3c2b5051749
Centurion256 <Vkuchynskyy@gmail.com>
2016-02-27 18:03:54 +0200

    Added files via upload

---
+++ Loops/Division.pas
@@ -0,0 +1,13 @@
+Program EvenNumbers;
+var
+    i : Integer;
+    k : Integer;
+    
+begin
+    writeln('¬каж≥ть значенн€ k');
+    read(k);
+   
+
+    for i:= 1 to k div 2 do
+        writeln(i*2);
+end.
