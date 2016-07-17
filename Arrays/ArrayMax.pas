e9c457fcee26fc2cf183e49c37af21f935389adf
Centurion256 <Vkuchynskyy@gmail.com>
2016-07-17 15:11:01 +0300

    Update ArrayMax.pas

--- Arrays/ArrayMax.pas
+++ Arrays/ArrayMax.pas
@@ -1,4 +1,4 @@
-Program HelloWorld(output);
+Program ArrayMax;
 const n=20;
 var
 i,max: integer;
@@ -6,21 +6,26 @@ A:array [1..n] of integer;
 
 begin
     randomize;
-    
+    //forming
+    for i:= 1 to n do
+    begin
+        A[i]:= round(random()*1000);
+    end;
+    //search for max
+    max:=A[1];
     for
     i:= 1 to n do
     begin
-        A[i]:= round(random()*1000);
         if  
         A[i] > max then
         max:= A[i]
     end;
-    
+    //output
     for
     i:= 1 to n do
     begin
-        write (A[i] : 5);
-    writeln; 
-        write (max);
+        writeln (A[i] : 5);
     end;
+    writeln ('max = ', max);
 end.
+
