64a6d853d3ee2431df0cb8f54ff2e3b69a727867
Centurion256 <Vkuchynskyy@gmail.com>
2016-07-23 17:49:49 +0300

    Created by Coding Ground Client

---
+++ JArrays/JBubbleSort.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+
+public class JBubbleSort{
+
+     public static void main(String []args){
+        final int N = 10;  
+        int A[] = new int [N];
+        //forming
+        Random gen = new Random();
+        for (int i = 0; i < N; i++) 
+        {
+            A[i]=gen.nextInt(1000);
+        }
+        
+        //Output
+        for (int i = 0; i < N; i++)
+        { 
+            System.out.format("%5d " , A[i]);
+        }
+        System.out.println();
+        
+        //BubbleSort
+        for (int k = 0; k < N-1; k++)
+            for (int i = 0; i < N-k-1; i++)
+            {
+                if (A[i] > A[i+1])
+                {
+                    int temp = A[i];
+                    A[i] = A[i+1];
+                    A[i+1] = temp;
+                }
+            }
+        //Output
+        for (int i = 0; i < N; i++)
+        { 
+            System.out.format("%5d " , A[i]);
+        }
+        System.out.println();
+    
+     }
+}     
