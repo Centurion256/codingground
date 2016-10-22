6b84c2f0a1a3ef83b03c8c92881c941791c70e8d
Centurion256 <Vkuchynskyy@gmail.com>
2016-10-22 21:05:42 +0300

    Update JBubbleSort.java

--- JArrays/JBubbleSort.java
+++ JArrays/JBubbleSort.java
@@ -1,17 +1,28 @@
 import java.util.Random;
 
 public class JBubbleSort{
+    public static void Forming(int []AR){ 
+            //forming
+        int N = AR.length;   
+        Random gen = new Random();
+        for (int i = 0; i < N; i++) 
+        {
+            AR[i]=gen.nextInt(1000);
+        }
+    
+    }
 
      public static void main(String []args){
         final int N = 10;  
         int A[] = new int [N];
-        //forming
+      /*  //forming
         Random gen = new Random();
         for (int i = 0; i < N; i++) 
         {
             A[i]=gen.nextInt(1000);
         }
-        
+        */
+        Forming(A);
         //Output
         for (int i = 0; i < N; i++)
         { 
